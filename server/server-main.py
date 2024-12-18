0bcf196ab39d4c2da5eac3621cd75435298d0642
ilankay <ilankayesar+git@gmail.com>
2024-12-19 00:19:49 +0200

    added basic interface for server

--- server/server-main.py
+++ server/server-main.py
@@ -5,14 +5,29 @@ import threading
 class CloseConn(Exception):
     pass
 
+def add_client(pub_key):
+    pass
+
+def add_work(pub_key,hash):
+    pass
+
+def block_found(pub_key,hash):
+    pass
+
 def handle_client(conn, addr):
-   data = None
-   while True:
-       data = conn.recv(1024)
-       conn.sendall(data)
+    data = None
+    while True:
+       data = conn.recv(4354)
        if data is None: break
-    
-    
+    pub_key = data[:512]
+    operation_code = data[512]>>6 
+    hash = (data[512:]<<6)>>6
+    match(operation_code):
+        case 0: return
+        case 1: add_work(pub_key,hash)
+        case 2: block_found(pub_key,hash)
+        case 3: add_client(pub_key)
+        
 def init_server(host, port):
     server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     server.bind((host, port))
