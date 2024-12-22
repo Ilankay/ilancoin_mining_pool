4fb81c1150ffa9693c03b2a4529857814a50f1f8
ilankay <ilankayesar+git@gmail.com>
2024-12-22 13:22:03 +0200

    changed from bits to bytes

--- server/server-main.py
+++ server/server-main.py
@@ -3,14 +3,14 @@ import socket
 import threading
 
 #gloabl constants
-VALID_SIZE = 4099
-PKEY_LEN = 512 #length of public key in bytes
+VALID_SIZE = 98
+PKEY_LEN = 65 #length of public key in bytes
 
 class CloseConn(Exception):
     """ an exception to throw in order to close a client connection"""
     pass
 
-class db_api:
+class DbApi:
     def __init__(self, db_path:str):
         self.db_path = db_path
         with sqlite3.connect(db_path) as conn:
@@ -24,15 +24,18 @@ class db_api:
     def add_client(self,pkey):
         pass
 
-class handle_client:
+class HandleClient:
 
     def add_client(self):
+        print("client added")
         pass
 
     def add_work(self):
+        print("work is added")
         pass
 
     def block_found(self):
+        print("block is found!")
         pass
     def validate_data(se1f,data)->bool:
         """the method returns true if data is valid and false if data is invalid"""
@@ -48,8 +51,8 @@ class handle_client:
             if data is None: break
         if not self.validate_data(data): raise CloseConn
         self.pub_key = data[:PKEY_LEN]
-        self.operation_code = data[PKEY_LEN]>>6 
-        self.hash = (data[PKEY_LEN:]<<6)>>6
+        self.operation_code = data[PKEY_LEN]
+        self.hash = data[PKEY_LEN:]
 
         #should be moved into seperate method
         match(self.operation_code):
@@ -58,10 +61,11 @@ class handle_client:
             case 2: self.block_found()
             case 3: self.add_client()
 
-def start_thread(conn, addr):
+def start_thread(conn, addr,db):
     """this starts the thread in order to handle a new client"""
     try:
-        client = handle_client(conn,addr)
+        print(f"client connected from address: {addr}")
+        client = HandleClient(conn,addr,db)
     except CloseConn:
         conn.shutdown()
         conn.close()
@@ -72,7 +76,7 @@ def init_server(host, port, db_path):
     server.listen()
 
     #create db object
-    db = db_api(db_path)
+    db = DbApi(db_path)
 
 
     clients = 0
@@ -80,12 +84,13 @@ def init_server(host, port, db_path):
     # handle clients
     while True:
         conn, addr = server.accept()
-        threads.append(threading.Thread(target=handle_client,args=(conn,addr,db)))
+        threads.append(threading.Thread(target=start_thread,args=(conn,addr,db)))
         threads[clients].start()
         clients+=1
 
 
 if __name__ == "__main__":
+    db_path = "work.db"
     HOST = "localhost"
     PORT = 1234
-    init_server(HOST,PORT)
+    init_server(HOST,PORT,db_path)
